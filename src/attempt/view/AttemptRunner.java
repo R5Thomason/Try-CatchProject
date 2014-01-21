24d3f7864206e10168c7c1a451d72b9e4db5f376
R5Thomason <ryanjonathanthomason@gmail.com>
2014-01-21 11:51:15 -0700

    this is for changing seats

---
+++ src/attempt/view/AttemptRunner.java
@@ -0,0 +1,43 @@
+package attempt.view;
+
+import javax.swing.JOptionPane;
+
+public class AttemptRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int testNumber = 15;
+		int otherNumber = 0;
+		double testDouble = 136.2312;
+		double otherDouble = 0.00;
+		
+		try
+		{
+			testNumber = testNumber/otherNumber;
+			JOptionPane.showMessageDialog(null, "Here is the answer:" + testNumber);
+			
+		}
+		catch(ArithmeticException currentError)
+		{
+			JOptionPane.showMessageDialog(null, "Here is the error" + currentError.getMessage());
+			JOptionPane.showMessageDialog(null, "Here is the error" + currentError.toString());
+		}
+		
+		try
+		{
+			testDouble = -testDouble/otherDouble;
+			JOptionPane.showMessageDialog(null, "Here is the answer:" + testDouble);
+			
+		}
+		catch(ArithmeticException currentError)
+		{
+			JOptionPane.showMessageDialog(null, "Here is the error" + currentError.getMessage());
+			JOptionPane.showMessageDialog(null, "Here is the error" + currentError.toString());
+		}
+	}
+
+}
